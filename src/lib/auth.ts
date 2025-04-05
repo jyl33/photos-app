e45b2c87f6058a8a2325305b9a7a36e9dfecfbd4
jyl33 <27440827+jyl33@users.noreply.github.com>
2025-04-05 10:31:00 +0800

    Updated auth database to supabase

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import { betterAuth } from "better-auth";
+/* import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
 import { nextCookies } from "better-auth/next-js";
@@ -14,4 +14,17 @@ export const auth = betterAuth({
     },
     trustedOrigins: ["http://localhost:3000", "https://7cd2-173-56-32-144.ngrok-free.app"],
     plugins: [nextCookies()] 
+}); */
+
+import { betterAuth } from 'better-auth';
+import { Pool } from 'pg';
+
+export const auth = betterAuth({
+    database: new Pool({
+        connectionString: process.env.DATABASE_URL
+    }),
+    secret: process.env.BETTER_AUTH_SECRET,
+    emailAndPassword: {  
+        enabled: true
+    },
 });
