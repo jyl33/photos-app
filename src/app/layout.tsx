cd56e6b375b5fc8f69a87b6639a963ce99f4bab3
jyl33 <27440827+jyl33@users.noreply.github.com>
2025-04-05 10:53:23 +0800

    added speed insights, removed sign up page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -25,6 +26,7 @@ export default async function RootLayout({
               </div>
             </div>
           <Toaster />
+          <SpeedInsights/>
       </body>
     </html>
   );
